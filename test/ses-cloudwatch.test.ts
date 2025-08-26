7e4f52752cfe6bd1b9016af49351e0549ca8a2d7
Pablo Cano <pablo.cano@merapar.com>
2025-08-26 11:09:55 -0400

    chore: changed lint format

--- test/ses-cloudwatch.test.ts
+++ test/ses-cloudwatch.test.ts
@@ -1,52 +1,52 @@
-import { App, Stack } from 'aws-cdk-lib';
-import { Template, Match } from 'aws-cdk-lib/assertions';
-import { SesCloudWatch } from '../src';
+import { App, Stack } from "aws-cdk-lib";
+import { Template, Match } from "aws-cdk-lib/assertions";
+import { SesCloudWatch } from "../src";
 
 // Helper to get synthesized template
 function synth(stack: Stack) {
   return Template.fromStack(stack);
 }
 
-describe('SesCloudWatch', () => {
-  test('defaults - auto names when not provided', () => {
+describe("SesCloudWatch", () => {
+  test("defaults - auto names when not provided", () => {
     const app = new App();
-    const stack = new Stack(app, 'TestStack');
+    const stack = new Stack(app, "TestStack");
 
-    new SesCloudWatch(stack, 'Subject');
+    new SesCloudWatch(stack, "Subject");
 
     const template = synth(stack);
 
     // Configuration Set should have derived name
-    template.hasResourceProperties('AWS::SES::ConfigurationSet', {
-      Name: 'TestStack-ses-config-set',
+    template.hasResourceProperties("AWS::SES::ConfigurationSet", {
+      Name: "TestStack-ses-config-set",
     });
 
     // LogGroup exists (name not specified => not asserting name)
-    template.resourceCountIs('AWS::Logs::LogGroup', 1);
+    template.resourceCountIs("AWS::Logs::LogGroup", 1);
 
     // EventBridge rule with correct pattern
-    template.hasResourceProperties('AWS::Events::Rule', {
-      EventPattern: Match.objectLike({ source: ['aws.ses'] }),
+    template.hasResourceProperties("AWS::Events::Rule", {
+      EventPattern: Match.objectLike({ source: ["aws.ses"] }),
     });
   });
 
-  test('custom names respected', () => {
+  test("custom names respected", () => {
     const app = new App();
-    const stack = new Stack(app, 'AnotherStack');
+    const stack = new Stack(app, "AnotherStack");
 
-    new SesCloudWatch(stack, 'Subject', {
-      logGroupName: 'custom-loggroup-name',
-      configurationSetName: 'custom-config-set',
+    new SesCloudWatch(stack, "Subject", {
+      logGroupName: "custom-loggroup-name",
+      configurationSetName: "custom-config-set",
     });
 
     const template = synth(stack);
 
-    template.hasResourceProperties('AWS::SES::ConfigurationSet', {
-      Name: 'custom-config-set',
+    template.hasResourceProperties("AWS::SES::ConfigurationSet", {
+      Name: "custom-config-set",
     });
 
-    template.hasResourceProperties('AWS::Logs::LogGroup', {
-      LogGroupName: 'custom-loggroup-name',
+    template.hasResourceProperties("AWS::Logs::LogGroup", {
+      LogGroupName: "custom-loggroup-name",
     });
   });
 });
